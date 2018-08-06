b003c244a99b8880ccda1f92c6a428c43ba6cc99
David <sylvio.brandon.david@accenture.com>
2018-08-06 11:20:37 +0400

    Project File Init

---
+++ springBoot/InterviewMgtProject/src/test/java/com/accenture/interviewproj/InterviewMgtProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.accenture.interviewproj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InterviewMgtProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
